e1b417a29352b04963fdc10253fc1c5e1c46871c
Mehdi Ben Hadj Ali <mehdibenhadjali@gmail.com>
2021-08-05 18:34:11 +0100

    initi of project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Loader from './components/Loader';
+
+function App() {
+  return (
+    <div className="App">
+      <Loader />
+
+    </div>
+  );
+}
+
+export default App;
